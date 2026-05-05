fbdb488afb67b097ea890b28977538d8c9541ac4
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-05 16:13:12 -0700

    made it have its own camera so it doesnt bug out

--- source/virt/Call.hx
+++ source/virt/Call.hx
@@ -1,15 +1,23 @@
 package virt;
 
 import flixel.FlxG;
+import flixel.FlxCamera;
 
 class Call {
     public static var mouse:VirtualCursor;
-
+    public static var mouseCam:FlxCamera;
     /**
-     * Usage: Call.Mouse();
-     */
+    usage: Call.Mouse();
+    **/
     public static function Mouse():Void {
+        mouseCam = new FlxCamera();
+        mouseCam.bgColor.alpha = 0;
+        FlxG.cameras.add(mouseCam, false);
+
         mouse = new VirtualCursor(FlxG.width / 2, FlxG.height / 2);
+        
+        mouse.cursorSprite.cameras = [mouseCam];
+        
         FlxG.state.add(mouse);
     }
 }
