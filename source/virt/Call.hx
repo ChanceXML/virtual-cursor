04bb693113080aa38e0f9c0ffd473f1f83ca39ff
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-05 18:57:26 -0700

    yeah

--- source/virt/Call.hx
+++ source/virt/Call.hx
@@ -6,18 +6,36 @@ import flixel.FlxCamera;
 class Call {
     public static var mouse:VirtualCursor;
     public static var mouseCam:FlxCamera;
-    /**
-    usage: Call.Mouse();
-    **/
+
     public static function Mouse():Void {
+        if (mouse != null) {
+            if (FlxG.plugins.list.contains(mouse)) {
+                FlxG.plugins.remove(mouse);
+            }
+            mouse.destroy();
+            mouse = null;
+        }
+
+        if (mouseCam != null && FlxG.cameras.list.contains(mouseCam)) {
+            FlxG.cameras.remove(mouseCam, false);
+            mouseCam = null;
+        }
+
         mouseCam = new FlxCamera();
-        mouseCam.bgColor.alpha = 0;
-        FlxG.cameras.add(mouseCam, false);
+        mouseCam.bgColor.alpha = 0; 
+        FlxG.cameras.add(mouseCam, false); 
 
         mouse = new VirtualCursor(FlxG.width / 2, FlxG.height / 2);
-        
         mouse.cursorSprite.cameras = [mouseCam];
         
-        FlxG.state.add(mouse);
+        FlxG.plugins.add(mouse);
+    }
+    
+    public static function kill():Void {
+        if (mouse != null) {
+            FlxG.plugins.remove(mouse);
+            mouse.destroy();
+            mouse = null;
+        }
     }
 }
