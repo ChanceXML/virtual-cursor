e0e837790def8bb444c7e8369f1e27a41d9ec4b2
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-11 16:18:20 -0700

    •

--- source/virt/VirtualCursor.hx
+++ source/virt/VirtualCursor.hx
@@ -3,260 +3,151 @@ package virt;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxBasic;
+import flixel.FlxCamera;
 import flixel.group.FlxGroup;
-import flixel.tweens.FlxTween;
-import flixel.tweens.FlxEase;
-import flixel.input.touch.FlxTouch;
 import flixel.math.FlxPoint;
 import flixel.math.FlxMath;
 import flixel.util.FlxDestroyUtil;
+import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+import flixel.input.touch.FlxTouch;
 import openfl.Lib;
-import flixel.text.FlxInputText; 
-import flixel.FlxCamera;
 
+/**
+ * Virtual Cursor
+ * Added as a Plugin to override all states/substates.
+ * Use VirtualCursor.init() to start and VirtualCursor.killCursor() to stop.
+ */
 @:keep
-class VirtualCursor extends FlxGroup
+class VirtualCursor extends FlxBasic
 {
+    public static var instance:VirtualCursor;
+
     public var cursorSprite:FlxSprite;
     public var sensitivity:Float = 1.5;
-    public var tapThreshold:Float = 20.0;
-    public var holdDelay:Float = 0.2;
+    public var tapThreshold:Float = 15.0;
     
-    private var isHovering:Bool = false;
-    private var _currentlyHovering:Bool = false;
-    private var _clickTween:FlxTween;
-    
-    private var _lastTouchPos:FlxPoint; 
-    private var _startTouchPos:FlxPoint;
-    private var _trackedTouchID:Int = -1; 
-    
-    private var _touchState:Int = 0;
-    private var _touchTimer:Float = 0;
-    private var _clickTimer:Float = 0;
-    
-    private static inline var CURSOR_IMG:String = "assets/images/menus/cursor/mouse.png";
-    private static inline var HOVER_IMG:String = "assets/images/menus/cursor/hover.png";
-
+    private var _lastTouchPos:FlxPoint;
+    private var _trackedTouchID:Int = -1;
     private var _cursorCamera:FlxCamera;
+    private var _clickTween:FlxTween;
 
-    public function new(startX:Float = 0, startY:Float = 0)
+    public static function init():Void {
+        if (instance == null) {
+            instance = new VirtualCursor();
+            FlxG.plugins.add(instance);
+        }
+    }
+    
+    public static function Kill():Void {
+        if (instance != null) {
+            FlxG.plugins.remove(instance);
+            instance.destroy();
+            instance = null;
+        }
+    }
+
+    public function new()
     {
         super();
-
-        _cursorCamera = new FlxCamera();
-        _cursorCamera.bgColor = 0x00000000;
+        
+        _cursorCamera = new FlxCamera(0, 0, FlxG.width, FlxG.height);
+        _cursorCamera.bgColor = FlxColor.TRANSPARENT;
         FlxG.cameras.add(_cursorCamera, false);
 
-        cursorSprite = new FlxSprite(startX, startY);
-        cursorSprite.loadGraphic(CURSOR_IMG);
+        cursorSprite = new FlxSprite(FlxG.width / 2, FlxG.height / 2);
+        cursorSprite.loadGraphic("assets/images/menus/cursor/mouse.png");
         cursorSprite.antialiasing = true;
-        cursorSprite.scrollFactor.set(0, 0); 
         cursorSprite.cameras = [_cursorCamera];
-        add(cursorSprite);
         
         _lastTouchPos = FlxPoint.get();
-        _startTouchPos = FlxPoint.get();
 
         #if mobile
         FlxG.mouse.visible = false;
-        FlxG.mouse.enabled = false; 
         #end
     }
 
     override public function update(elapsed:Float):Void
     {
-        handleTouchInput(elapsed);
-        
-        FlxG.mouse.setGlobalScreenPositionUnsafe(cursorSprite.x, cursorSprite.y);
-
-        if (_clickTimer > 0)
-        {
-            _clickTimer -= elapsed;
-            if (_clickTimer <= 0)
-            {
-                handleRelease();
-            }
-        }
-
-        if (FlxG.cameras.list.indexOf(_cursorCamera) < FlxG.cameras.list.length - 1)
-        {
+        if (FlxG.cameras.list.indexOf(_cursorCamera) < FlxG.cameras.list.length - 1) {
             FlxG.cameras.remove(_cursorCamera, false);
             FlxG.cameras.add(_cursorCamera, false);
         }
 
-        updateHoverLogic();
+        handleTouchInput();
+        
+        FlxG.mouse.setGlobalScreenPositionUnsafe(cursorSprite.x, cursorSprite.y);
+
         super.update(elapsed);
     }
 
-    private function handleTouchInput(elapsed:Float):Void 
+    private function handleTouchInput():Void 
     {
         var activeTouch:FlxTouch = null;
 
-        if (_trackedTouchID != -1) 
-        {
-            for (touch in FlxG.touches.list) 
-            {
-                if (touch.touchPointID == _trackedTouchID) 
-                {
+        if (_trackedTouchID != -1) {
+            for (touch in FlxG.touches.list) {
+                if (touch.touchPointID == _trackedTouchID) {
                     activeTouch = touch;
                     break;
                 }
             }
-            
-            if (activeTouch == null) 
-            {
-                if (_touchState == 3) handleRelease();
-                _touchState = 0;
-                _trackedTouchID = -1;
-            }
-        }
-
-        if (activeTouch == null) 
-        {
-            for (touch in FlxG.touches.list) 
-            {
-                if (touch.justPressed) 
-                {
+        } else {
+            for (touch in FlxG.touches.list) {
+                if (touch.justPressed) {
                     activeTouch = touch;
                     _trackedTouchID = touch.touchPointID;
                     _lastTouchPos.set(touch.screenX, touch.screenY);
-                    _startTouchPos.set(touch.screenX, touch.screenY);
-                    _touchState = 1;
-                    _touchTimer = 0;
                     break;
                 }
             }
         }
-        
-        if (activeTouch != null) 
-        {
-            if (activeTouch.pressed || activeTouch.justReleased) 
-            {
-                var dx:Float = activeTouch.screenX - _lastTouchPos.x;
-                var dy:Float = activeTouch.screenY - _lastTouchPos.y;
-                
-                cursorSprite.x = FlxMath.bound(cursorSprite.x + dx * sensitivity, 0, FlxG.width);
-                cursorSprite.y = FlxMath.bound(cursorSprite.y + dy * sensitivity, 0, FlxG.height);
-                
-                _lastTouchPos.set(activeTouch.screenX, activeTouch.screenY);
-            }
 
-            if (activeTouch.pressed && _touchState == 1)
-            {
-                _touchTimer += elapsed;
-                var distX = activeTouch.screenX - _startTouchPos.x;
-                var distY = activeTouch.screenY - _startTouchPos.y;
-                var distFromStart = Math.sqrt(distX * distX + distY * distY);
-
-                if (distFromStart > tapThreshold)
-                {
-                    _touchState = 2;
-                }
-                else if (_touchTimer >= holdDelay)
-                {
-                    _touchState = 3;
-                    handlePress();
-                }
-            }
+        if (activeTouch != null) {
+            var dx:Float = activeTouch.screenX - _lastTouchPos.x;
+            var dy:Float = activeTouch.screenY - _lastTouchPos.y;
             
-            if (activeTouch.justReleased) 
-            {
-                if (_touchState == 1)
-                {
-                    performQuickClick();
-                }
-                else if (_touchState == 3)
-                {
-                    handleRelease();
-                }
-                
-                _touchState = 0;
+            cursorSprite.x = FlxMath.bound(cursorSprite.x + dx * sensitivity, 0, FlxG.width);
+            cursorSprite.y = FlxMath.bound(cursorSprite.y + dy * sensitivity, 0, FlxG.height);
+            
+            _lastTouchPos.set(activeTouch.screenX, activeTouch.screenY);
+
+            if (activeTouch.justPressed) {
+                handlePress();
+            }
+            if (activeTouch.justReleased) {
+                handleRelease();
                 _trackedTouchID = -1;
             }
         }
-    }
-
-    private function performQuickClick():Void
-    {
-        handlePress();
-        _clickTimer = 0.05;
     }
 
     private function handlePress():Void
     {
         if (_clickTween != null) _clickTween.cancel();
-        cursorSprite.scale.set(0.7, 0.7);
-        
+        cursorSprite.scale.set(0.8, 0.8);
         @:privateAccess FlxG.mouse._leftButton.press();
-        checkForInputText();
     }
 
     private function handleRelease():Void
     {
         if (_clickTween != null) _clickTween.cancel();
-        _clickTween = FlxTween.tween(cursorSprite.scale, {x: 1, y: 1}, 0.15, {ease: FlxEase.backOut});
-
+        _clickTween = FlxTween.tween(cursorSprite.scale, {x: 1, y: 1}, 0.1, {ease: FlxEase.quartOut});
         @:privateAccess FlxG.mouse._leftButton.release();
     }
 
-    private function updateHoverLogic():Void
-    {
-        _currentlyHovering = false;
-
-        if (FlxG.state != null)
-        {
-            FlxG.state.forEachOfType(FlxSprite, checkHoverOverlap);
-            if (FlxG.state.subState != null)
-                FlxG.state.subState.forEachOfType(FlxSprite, checkHoverOverlap);
-        }
-
-        if (_currentlyHovering != isHovering)
-        {
-            isHovering = _currentlyHovering;
-            cursorSprite.loadGraphic(isHovering ? HOVER_IMG : CURSOR_IMG);
-        }
+    override public function draw():Void {
+        cursorSprite.draw();
+        super.draw();
     }
 
-    private function checkHoverOverlap(spr:FlxSprite):Void
-    {
-        if (_currentlyHovering || spr == null || !spr.visible || !spr.exists || spr == cursorSprite)
-            return;
-        
-        if (FlxG.mouse.overlaps(spr, spr.camera))
-        {
-            _currentlyHovering = true;
-        }
-    }
-    
-    private function checkForInputText():Void 
-    {
-        var check = function(obj:FlxBasic) {
-            if (obj != null && obj.visible && obj.exists && Std.isOfType(obj, FlxInputText)) {
-                var input:FlxInputText = cast obj;
-                if (FlxG.mouse.overlaps(input, input.camera)) {
-                    @:privateAccess input.hasFocus = true;
-                    Lib.application.window.textInputEnabled = true;
-                }
-            }
-        };
-
-        FlxG.state.forEach(check, true);
-        if (FlxG.state.subState != null) FlxG.state.subState.forEach(check, true);
-    }
-    
     override public function destroy():Void 
     {
+        FlxG.cameras.remove(_cursorCamera);
         _lastTouchPos = FlxDestroyUtil.put(_lastTouchPos);
-        _startTouchPos = FlxDestroyUtil.put(_startTouchPos);
         if (_clickTween != null) _clickTween.cancel();
-        
-        #if mobile
-        FlxG.mouse.enabled = true; 
-        #end
-        
-        if (_cursorCamera != null)
-            FlxG.cameras.remove(_cursorCamera);
-            
+        cursorSprite.destroy();
         super.destroy();
     }
 }
